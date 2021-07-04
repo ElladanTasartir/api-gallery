b186a98740cf509723262ab81c6ad6d86bb8422c
Erick Malta <erickmalta100@gmail.com>
2021-07-04 00:40:23 -0300

    feat(gallery): add gallery module

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { UploadModule } from 'src/upload/upload.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GallerySchema } from 'src/schemas/gallery.schema';
+import { UserModule } from 'src/user/user.module';
+import { UserSchema } from '../schemas/user.schema';
+import { UploadModule } from '../upload/upload.module';
 import { GalleryController } from './gallery.controller';
+import { GalleryRepository } from './gallery.repository';
 import { GalleryService } from './gallery.service';
 
 @Module({
-  imports: [UploadModule],
+  imports: [
+    MongooseModule.forFeature([{
+      name: 'User',
+      schema: UserSchema,
+      collection: 'users',
+    }, {
+      name: 'Gallery',
+      schema: GallerySchema,
+    }]),
+    UserModule,
+    UploadModule
+  ],
   controllers: [GalleryController],
-  providers: [GalleryService]
+  providers: [GalleryService, GalleryRepository]
 })
-export class GalleryModule {}
+export class GalleryModule { }
