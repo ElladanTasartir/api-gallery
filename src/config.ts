0b69a15892783fe59c19685ef1e21c429823d3b6
Erick Malta <erickmalta100@gmail.com>
2021-07-04 00:21:13 -0300

    feat(config): add jwt vars

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,8 @@ config();
 export const port = process.env.PORT || 3000;
 
 export const mongoDBUri = `mongodb://${process.env.MONGODB_URI}`;
+
+export const jwt = {
+  secret: process.env.JWT_SECRET || 'secret',
+  expiresIn: process.env.EXPIRES_IN,
+};
