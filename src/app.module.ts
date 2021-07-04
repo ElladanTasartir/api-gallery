88044dc6e8ae89b7d3c06e2500c483847b012ee3
Erick Malta <erickmalta100@gmail.com>
2021-07-04 00:21:37 -0300

    feat(app): add static serving files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { GalleryModule } from './gallery/gallery.module';
 import { UploadModule } from './upload/upload.module';
 import { UserModule } from './user/user.module';
@@ -7,6 +9,9 @@ import { mongoDBUri } from './config';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..', 'tmp', 'images'),
+    }),
     MongooseModule.forRoot(
       mongoDBUri, {
       useNewUrlParser: true,
